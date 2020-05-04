2748bd22128dc5ae841d2c455caeec6fa4ef5fe5
Mick <mgrove@gmail.com>
2020-05-04 11:46:11 -0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Install command-line tools using Homebrew.
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Make sure we’re using the latest Homebrew.
 brew update

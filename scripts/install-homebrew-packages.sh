9d90994ff62680ed925d968d9a2c7419a85e8bad
Mick Grove <mickgrove@apple.com>
2021-01-03 13:42:16 -0800

    more updates

--- scripts/install-homebrew-packages.sh
+++ scripts/install-homebrew-packages.sh
@@ -2,16 +2,15 @@
 source ./functions/common
 
 brew tap homebrew/cask-fonts
-brew tap homebrew/homebrew-cask-fonts/
 
 # Install long list of packages.
 IFS=$'\n'
 
 print_line "Installing Homebrew packages."
-cat ../lists/brew-packages | xargs brew install
+cat ./lists/brew-packages | xargs brew install
 
 print_line "Installing Homebrew cask packages."
-cat ../lists/brew-cask-packages | xargs brew install --cask
+cat ./lists/brew-cask-packages | xargs brew install --cask
 
 #print_line "Installing App Store applications."
 #cat ../lists/mas-app-ids | xargs mas install
